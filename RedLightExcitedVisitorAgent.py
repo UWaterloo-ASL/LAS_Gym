29ccd3f57e83a5fb40c53b62b2278a2d91b3eebe
Lingheng Meng <linghengmeng@yahoo.com>
2018-05-08 22:48:42 -0400

    if new target position is very close to last target position, don't move.

--- RedLightExcitedVisitorAgent.py
+++ RedLightExcitedVisitorAgent.py
@@ -90,14 +90,22 @@ class RedLightExcitedVisitorAgent():
             distance <= self._distanceThreshold and \
             self._lastTargetPositionMaintainCounter > self._lastTargetPositionMaintainThreshold:
             move = 1
-            print("Red light number: {}".format(self.red_light_num))
+            #print("Red light number: {}".format(self.red_light_num))
             random_red_light = np.random.randint(0,self.red_light_num)
             position = red_light_positions[random_red_light,:]
-            # each time change destination, _lastDestination will be updated 
-            self._lastDestination = position[0:2] #ignore z coordinate
-            self._lastTargetPositionMaintainCounter = 1
-            print("Visitor Destination:{}".format(self._lastDestination))
-            action = np.concatenate(([move], position.flatten()))    
+            newTargetPosition = position[0:2]
+            # if new target position is very close to last target location, do
+            # not move.
+            if self._distance_lastDestination_currLocation(newTargetPosition,self._lastDestination) < self._distanceThreshold:
+                move = 0
+                action = [move, 0, 0, 0]
+                self._lastTargetPositionMaintainCounter += 1 # increase one
+            else:
+                # each time change destination, _lastDestination will be updated 
+                self._lastDestination = position[0:2] #ignore z coordinate
+                self._lastTargetPositionMaintainCounter = 1
+                #print("Visitor Destination:{}".format(self._lastDestination))
+                action = np.concatenate(([move], position.flatten()))    
         else:
             move = 0
             action = [move, 0, 0, 0]
