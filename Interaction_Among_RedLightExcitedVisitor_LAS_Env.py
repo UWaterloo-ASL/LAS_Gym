119f7fc43386a92b562299545026da77f25fa248
Lingheng Meng <linghengmeng@yahoo.com>
2018-05-09 10:24:52 -0400

    two interactions in one script

--- Interaction_Among_RedLightExcitedVisitor_LAS_Env.py
+++ Interaction_Among_RedLightExcitedVisitor_LAS_Env.py
@@ -7,10 +7,13 @@ Created on Wed May  2 17:46:02 2018
 """
 
 import time
+import multiprocessing as mp
 
 from RedLightExcitedVisitorAgent import RedLightExcitedVisitorAgent
 from RedLightExcitedVisitor_LAS_Env import LivingArchitectureEnv
 from LASAgent import LASAgent
+
+
 
 if __name__ == '__main__':
     
@@ -21,9 +24,10 @@ if __name__ == '__main__':
     visitor = RedLightExcitedVisitorAgent("Visitor#0")
     
     # Instantiate environment object
-    env = LivingArchitectureEnv()
+    envLAS = LivingArchitectureEnv('127.0.0.1', 19997)
+    envVisitor = LivingArchitectureEnv('127.0.0.1', 19999)
     
-    observationForLAS, observationForVisitor, rewardLAS, rewardVisitor, done, [] = env.reset_env_for_LAS_red_light_excited_visitor(visitor._bodayName)
+    observationForLAS, observationForVisitor, rewardLAS, rewardVisitor, done, [] = envLAS.reset_env_for_LAS_red_light_excited_visitor(visitor._bodayName)
     
     # Step counter
     i = 1
@@ -31,12 +35,13 @@ if __name__ == '__main__':
     while not done:
         try:
             action_LAS = LASAgent1.perceive_and_act(observationForLAS, rewardLAS, done)
-            observationForLAS, rewardLAS, done, info = env.step_LAS(action_LAS)
+            observationForLAS, rewardLAS, done, info = envLAS.step_LAS(action_LAS)
             #print("Step: {}, reward: {}".format(i, rewardLAS))
             
             targetPositionName, bodyName, action_visitor = visitor.perceive_and_act(observationForVisitor,rewardVisitor,done)
-            observationForVisitor, reward, done, [] = env.step_red_light_excited_visitor(targetPositionName, bodyName, action_visitor)
+            observationForVisitor, reward, done, [] = envVisitor.step_red_light_excited_visitor(targetPositionName, bodyName, action_visitor)
         except KeyboardInterrupt: # Ctrl-C stop interaction
             pass
         
-    env.destroy()
+    envVisitor.destroy()
+    envLAS.destroy()
