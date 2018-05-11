52e86e7cb4e97dc61cbe019156225228e74ed103
Lingheng Meng <linghengmeng@yahoo.com>
2018-05-11 15:07:01 -0400

    script used to set objects position
    later we can work on this script to avoid manually setting objects, because setting many nodes is really time-consuming.

---
+++ vrep_load_object_setObjectPosition.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 10:30:46 2018
+
+@author: jack.lingheng.meng
+"""
+
+"""
+We should write a script to automatically set all objects' position, rather than 
+manually drag and tune which is time-consuming.
+"""
