d8386b30bf5137504caed2656bec00f36a265d99
Lingheng Meng <linghengmeng@yahoo.com>
2018-08-11 10:59:38 -0400

    t

---
+++ Interaction_RandomLASAgent_and_Env.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 08:52:36 2018
+
+@author: jack.lingheng.meng
+"""
+
+from Environment.LASEnv import LASEnv
+from LASAgent.RandomLASAgent import RandomLASAgent
+
+if __name__ == '__main__':
+    
+    # Instantiate environment object
+    envLAS = LASEnv('127.0.0.1', 19997)
+    
+    observationForLAS= envLAS.reset()
+
+    # Iinstantiate LAS-agent
+    LASAgent1 = RandomLASAgent(envLAS.observation_space, envLAS.action_space)
+    
+    # Step counter
+    i = 1
+    done = False
+    rewardLAS = 0
+    while not done:
+
+        actionLAS = LASAgent1.interact(observationForLAS, rewardLAS, done)
+        observationForLAS, rewardLAS, done, info = envLAS.step(actionLAS)
+        print("LAS Step: {}, reward: {}".format(i, rewardLAS))
+        i += 1
+    
+    envLAS.destroy()
