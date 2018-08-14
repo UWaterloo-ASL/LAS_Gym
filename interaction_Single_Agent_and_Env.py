52e35379ff5aa0cec3b2ce9933d4204170be4e2f
Lingheng Meng <linghengmeng@yahoo.com>
2018-08-14 10:23:23 -0400

    add summary of bright light number

--- interaction_Single_Agent_and_Env.py
+++ interaction_Single_Agent_and_Env.py
@@ -11,10 +11,48 @@ Created on Wed Aug  8 19:35:51 2018
 import tensorflow as tf
 import numpy as np
 import time
+from datetime import datetime
+import os
 
 from Environment.LASEnv import LASEnv
 from LASAgent.InternalEnvOfAgent import InternalEnvOfAgent
 
+def calculate_total_bright_light_number(light_intensity,
+                                        bright_light_threshold = 0.95):
+    """
+    calculate the # of bright light.
+    
+    Parameters
+    ----------
+    light_intensity: array
+        
+    bright_light_threshold: float default = 0.95
+        
+    """
+    bright_light_number = 0
+    for intensity in light_intensity:
+        if intensity >= bright_light_threshold:
+            bright_light_number += 1
+    return bright_light_number
+
+def _init_summarize_total_bright_light_number():
+        """
+        Summarize the # of bright light.
+        """
+        bright_light_number = tf.placeholder(dtype = tf.float32)
+        bright_light_number_sum = tf.summary.scalar('total_bright_light_number', bright_light_number)
+        bright_light_number_sum_op = tf.summary.merge([bright_light_number_sum])
+        return bright_light_number_sum_op, bright_light_number
+# Summary directory
+save_dir = os.path.join(os.path.abspath('..'),'ROM_Experiment_results', 'Overall_Summary_Single_Agent')
+summary_dir = os.path.join(save_dir,datetime.now().strftime("%Y%m%d-%H%M%S"))
+if not os.path.isdir(summary_dir):
+    os.makedirs(summary_dir)
+
+tf_writer = tf.summary.FileWriter(summary_dir)
+# Summarize # of bright light
+total_bright_light_number_sum_op,\
+total_bright_light_number_sum = _init_summarize_total_bright_light_number()
 
 if __name__ == '__main__':
     
@@ -43,6 +81,19 @@ if __name__ == '__main__':
             # delay the observing of consequence of LASAgent's action
             observation_For_LAS, reward_for_LAS, done, info = envLAS.step(actionLAS)
             print("LAS Step: {}, reward: {}".format(i, reward_for_LAS))
+            
+            ###################################################################
+            #                          Summary                                #
+            ###################################################################
+            # This is a cheating function and only for analysis, because light
+            # intensity is not perceived by any sensor.
+            light_intensity = envLAS._get_all_light_intensity()
+            bright_light_number = calculate_total_bright_light_number(light_intensity)
+            # Summary total bright light number
+            summary_str_bright_light_number = sess.run(total_bright_light_number_sum_op, 
+                                                       feed_dict={total_bright_light_number_sum:bright_light_number})
+            tf_writer.add_summary(summary_str_bright_light_number, i)
+            ###################################################################
             
             i += 1
         
